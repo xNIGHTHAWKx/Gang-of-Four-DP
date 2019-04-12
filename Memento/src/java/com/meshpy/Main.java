fec8e58d3c08beb2e0d6e3bd27dab36b5e386ab1
Patryk Gitner <209317@edu.p.lodz.pl>
2019-04-12 19:51:40 +0200

    Dodanie wzorca kreacyjnego Memento

---
+++ Memento/src/java/com/meshpy/Main.java
@@ -0,0 +1,28 @@
+package com.meshpy;
+
+import com.meshpy.memento.CareTaker;
+import com.meshpy.memento.Originator;
+
+public class Main {
+    public static void main(String[] args) {
+        Originator originator = new Originator();
+        CareTaker careTaker = new CareTaker();
+
+        originator.setState("State #1");
+        originator.setState("State #2");
+        careTaker.add(originator.saveStateToMemento());
+
+        originator.setState("State #3");
+        careTaker.add(originator.saveStateToMemento());
+
+        originator.setState("State #4");
+        careTaker.add(originator.saveStateToMemento());
+        System.out.println("Current State: " + originator.getState());
+
+        originator.getStateFromMemento(careTaker.get(0));
+        System.out.println("First saved State: " + originator.getState());
+
+        originator.getStateFromMemento(careTaker.get(1));
+        System.out.println("Second saved State: " + originator.getState());
+    }
+}
---
+++ Memento/src/java/com/meshpy/memento/Memento.java
@@ -0,0 +1,14 @@
+package com.meshpy.memento;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
